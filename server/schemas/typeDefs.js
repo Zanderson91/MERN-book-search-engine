b26aee93b7d17df3e5922e17d680af43fb3a635b
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-15 17:22:05 -0500

    Query, Auth and Mutation set for all typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = 'gql'
+const typeDefs = gql`
 
     type Book {
         bookId: String
@@ -24,4 +24,20 @@ const typeDefs = 'gql'
         title: String
         image: String
         link: String
+}
+        type Query {
+        me: User
     }
+    type Auth {
+        token: ID
+        user: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: bookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs; 
