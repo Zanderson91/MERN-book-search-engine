52b16a7eea04d7ab73cae74e46e23bdad7087149
Zack Anderson <zack.anderson2011@gmail.com>
2021-10-15 07:06:50 -0500

    added imports for USER and useMutation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
+import { ADD_USER } from "../../utils/mutations";
+import { useMutation } from "@apollo/react-hooks";
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
